0b5b2e6cdec4f71ed32914dae16c118058016301
MDCL <morgan.leitao@gmail.com>
2023-12-02 22:05:37 +0000

    ok

--- start.py
+++ start.py
@@ -1,4 +1,6 @@
 import asyncio
+import os
+import shutil
 
 import nltk
 
@@ -13,12 +15,19 @@ from datetime import datetime
 start = datetime.now()
 config = import_config()
 
-def get_all():
+def delete_datasets_folder(config: dict):
+    path = config['path_streamlit']
+    if os.path.exists(path) and os.path.isdir(path):
+        shutil.rmtree(path)
+        print("Dossier 'datasets' supprimé.")
+
+def get_all(config: dict):
+    delete_datasets_folder(config)
     logging.info("Start creating dataframe for Machine Learning...")
     asyncio.run(machine_learning(config))
     all_for_one = GetDataframes(config)
     all_for_one.get_all_dataframes()
 
-get_all()
+get_all(config)
 end = datetime.now()
 print(f"Temps écoulé -> {end - start}")
