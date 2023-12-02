58668a5b0a926920d9afd0adfbda5cc58c5ebe7a
MDCL <morgan.leitao@gmail.com>
2023-12-02 21:28:14 +0000

    update_movies : Sat Dec  2 21:28:14 UTC 2023

--- run.sh
+++ run.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Naviguer au répertoire de votre projet
-cd /chemin/vers/votre/projet
+cd ~/scalper/digital_dreamers
 
 # Activer l'environnement Python
-source /chemin/vers/env/bin/activate
+source .venv/bin/activate
 
 # Exécuter le script Python
-python /chemin/vers/votre/script.py
+python start.py
 
 # Ajouter les changements à Git
 git add -A
 
 # Commit les changements avec la date et l'heure actuelle comme message
-git commit -m "Mise à jour de la base de données : $(date)"
+git commit -m "update_movies : $(date)"
 
 # Pousser les changements sur GitHub
 git push origin main
