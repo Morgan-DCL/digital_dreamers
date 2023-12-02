eba90979364efe8f621e4eba2716baf8314868cd
MDCL <morgan.leitao@gmail.com>
2023-12-02 22:15:28 +0000

    update_movies : Sat Dec  2 22:15:28 UTC 2023

--- start.py
+++ start.py
@@ -11,18 +11,10 @@ from utils.tools import import_config, logging
 nltk.download("stopwords")
 nltk.download("wordnet")
 from datetime import datetime
-
-start = datetime.now()
 config = import_config()
 
-# def delete_datasets_folder(config: dict):
-#     path = config['path_streamlit']
-#     if os.path.exists(path) and os.path.isdir(path):
-#         shutil.rmtree(path)
-#         print("Dossier 'datasets' supprimé.")
-
+start = datetime.now()
 def get_all(config: dict):
-    # delete_datasets_folder(config)
     logging.info("Start creating dataframe for Machine Learning...")
     asyncio.run(machine_learning(config))
     all_for_one = GetDataframes(config)
