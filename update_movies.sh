0b486f84b7180cf01be8bd0382ed89c79c365f82
MDCL <morgan.leitao@gmail.com>
2023-12-02 23:02:08 +0000

    up

--- update_movies.sh
+++ update_movies.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
-
-# Naviguer au répertoire de votre projet
 cd ~/scalper/digital_dreamers
-
-# Activer l'environnement Python
 source .venv/bin/activate
-
-# Exécuter le script Python
 python start.py
 
-# Ajouter les changements à Git
 git add -A
-
-# Commit les changements avec la date et l'heure actuelle comme message
 git commit -m "update_movies : $(date)"
-
-# Pousser les changements sur GitHub
 git push origin main
-
 git pull
