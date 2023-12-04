871fecb9dc32da9c94fe9f0b3c4705053c862f21
Morgan-DCL <morgan.leitao@gmail.com>
2023-12-04 22:43:08 +0000

    update_movies : Mon Dec  4 22:43:08 UTC 2023

--- start.py
+++ start.py
@@ -1,20 +1,22 @@
 import asyncio
 import os
 import shutil
+import numpy as np
 
 import nltk
 
 from get_dataframes.api_tmdb import main as machine_learning
 from get_dataframes.get_dataframes import GetDataframes
 from utils.tools import import_config, logging
-
+import json
 nltk.download("stopwords")
 nltk.download("wordnet")
 from datetime import datetime
 config = import_config()
-
+name_ = np.random.random()
 start = datetime.now()
 def get_all(config: dict):
+    logging.info(f"{name_})
     logging.info("Start creating dataframe for Machine Learning...")
     asyncio.run(machine_learning(config))
     all_for_one = GetDataframes(config)
